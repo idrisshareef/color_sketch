9e9d9e6ce5c2467bf8fbcbd91869eb6dd87dd36f
Sara Busby <sarbu9@yahoo.com>
2017-06-07 13:01:19 -0600

    Gave images an argument to be loaded onto the screen

--- curvesDrawing_js/sketch3/sketch.js
+++ curvesDrawing_js/sketch3/sketch.js
@@ -7,29 +7,29 @@ var star2;
 var star3;
 
 function setup() {
-    createCanvas(600, 400);
-    background(c);
+  createCanvas(600, 400);
+  background(c);
 	star1= loadImage("curvesYellowGreen.odt");
 	star2= loadImage("curvesYellowGreen2.odt");
 	star3= loadImage("curvesYellowGreen3.odt");
-	txt1= createP("something about the images");
-	txt2= createP("something else");
-	txt3= createP("something else");
+	txt1= createP("something about the images", 100, height-20);
+	txt2= createP("something else", 160, height-20);
+	txt3= createP("something else", 220, height-20);
 }
 
 function draw() {
-	createImage(star1, 20, 20);
+	star1.show;
 	txt1.mouseOver(flipto2);
 	txt2.mouseOver(flipto3);
 	txt3.mouseOver(flipto1);
 }
 
 function flipto2(){
-	createImage(star2, 20, 20);
+	star2.show;
 }
 function flipto3(){
-	createImage(star3, 20, 20);
+	star3.show;
 }
 function flipto1(){
-	createImage(star1, 20, 20);
+	star1.show;
 }
