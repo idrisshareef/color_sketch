f05e657d0ffb39c9c3e27d41908430949711c4e9
sarbu9 <sarbu9@yahoo.com>
2017-12-10 12:26:53 -0700

    Initial Commit

---
+++ rgb_js/sketch.js
@@ -0,0 +1,20 @@
+var col=0;
+
+function setup() {
+    createCanvas(340, 260);
+	print("setup function!");
+	frameRate(5);
+}
+
+function draw() {
+    background(120,30,col);
+    
+	if(col<255){
+        col+=1;
+	}
+	console.log(col);
+}
+
+function mousePressed(){
+	col = 250;
+}
