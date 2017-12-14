2466995d6cab123eb4a4e65eaf8b0b7b54966753
sarbu9 <sarbu9@yahoo.com>
2017-12-14 08:57:14 -0700

    webGL and added a rectangle.

--- rgb_js/sketch.js
+++ rgb_js/sketch.js
@@ -1,7 +1,7 @@
 var col= 30;
 
 function setup() {
-    createCanvas(340, 260);
+    createCanvas(340, 260, webGL);
 	print("setup function!");
 	frameRate(5);
 }
@@ -9,12 +9,14 @@ function setup() {
 function draw() {
     background(120,30,col);
     
-	if(col<255){
+    if(col<255){
         col+=1;
 	}
-	console.log(col);
+    console.log(col);
 }
 
 function mousePressed(){
 	col = 250;
+	fill(255);
+	rect(0, 0, 40, 30);
 }
